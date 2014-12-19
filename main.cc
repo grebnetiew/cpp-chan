da01127fb58b5e4d4535f88208f83e2067865e37
Erik Weitenberg <eweitenberg@gmail.com>
2014-12-19 14:11:44 +0100

    forgot the join; some extra tests it seems op~ is broken and always gives Fa-- oh fuck no, it works correctly, the other thread is still sleeping.

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 void work(chan<int> out) {
-    for(size_t i = 0; i != 5; ++i) {
+    for(size_t i = 0; i != 6; ++i) {
         out << i;
         this_thread::sleep_for(chrono::seconds(1));
     }
@@ -15,9 +15,18 @@ int main() {
     chan<int> ch;   // synchronous
     thread worker(work, ref(ch));
 
-    while(true) {
+    for(size_t i = 0; i != 5; ++i) {
         int output;
         ch >> output;
         cout << "Received a " << output << '\n';
     }
+
+    auto conditionalInt(~ch);
+    if (conditionalInt.first){
+        cout << "WTF? There is still a " << conditionalInt.second << '\n';
+    } else {
+        cout << "No more ints...\n";
+    }
+
+    worker.join();
 }
