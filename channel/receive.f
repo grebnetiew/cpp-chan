1fc5adf14d935cf26f2558770309d92b5c93845b
Erik <e.r.a.weitenberg@rug.nl>
2014-12-19 15:24:29 +0100

    slight improvements to exception safety

--- channel/receive.f
+++ channel/receive.f
@@ -3,15 +3,20 @@ T Channel<T>::receive() {
     std::unique_lock<std::mutex> ul(d_mutex);
     ++d_receivers_present;
 
-    d_senders.notify_all();             // Notify senders I'm here
-    while (size() == 0) {               // Wait until item
-        d_receivers.wait(ul);
-    }
-    // Locked; there is an item
+    try {
+        d_senders.notify_all();             // Notify senders I'm here
+        while (size() == 0) {               // Wait until item
+            d_receivers.wait(ul);
+        }
+        // Locked; there is an item
 
-    auto storage = std::unique_ptr<std::deque<T>>::get();
-    T result(std::move(storage->front()));   // Take the item
-    storage->pop_front();
-    --d_receivers_present;
-    return result;
+        auto storage = std::unique_ptr<std::deque<T>>::get();
+        T result(std::move(storage->front()));   // Take the item
+        storage->pop_front();
+        --d_receivers_present;
+        return result;
+    } catch (...) {
+        --d_receivers_present;
+        throw;
+    }
 }
--- channel/tryreceive.f
+++ channel/tryreceive.f
@@ -3,21 +3,25 @@ bool Channel<T>::try_receive(T &result) {
     std::unique_lock<std::mutex> ul(d_mutex);
     ++d_receivers_present;
 
-    d_senders.notify_all();                 // Notify senders I'm here
+    try {
+        d_senders.notify_all();                 // Notify senders I'm here
+        ul.unlock();
+        std::this_thread::yield();
+        ul.lock();
 
-    ul.unlock();
-    std::this_thread::yield();
-    ul.lock();
+        if (size() == 0) {
+            --d_receivers_present;              // within try, but can't throw
+            return false;
+        }
+        // Locked; there is an item
 
-    if (size() == 0) {
+        auto storage = std::unique_ptr<std::deque<T>>::get();
+        result = std::move(storage->front());   // Take the item
+        storage->pop_front();
         --d_receivers_present;
-        return false;
+        return true;
+    } catch (...) {
+        --d_receivers_present;
+        throw;
     }
-    // Locked; there is an item
-
-    auto storage = std::unique_ptr<std::deque<T>>::get();
-    result = std::move(storage->front());   // Take the item
-    storage->pop_front();
-    --d_receivers_present;
-    return true;
 }
