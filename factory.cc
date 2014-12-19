d55cc2cfd5e8870cd7a328d658eb86521321ae91
Erik Weitenberg <eweitenberg@gmail.com>
2014-12-19 18:59:06 +0100

    now no more memory leaks and safe thread exit

--- factory.cc
+++ factory.cc
@@ -4,38 +4,25 @@
 #include "chan/chan.h"
 using namespace std;
 
-void builder(chan<string> in, chan<string> out) {
+void worker(chan<string> in, chan<string> out, string const &text) 
+try {
     while(true) {
         string product;
         in >> product;
-        product += "two-story ";
+        product += text;
         out << product;
     }
-}
-void painter(chan<string> in, chan<string> out) {
-    while(true) {
-        string product;
-        in >> product;
-        product += "blue ";
-        out << product;
-    }
-}
-void finisher(chan<string> in, chan<string> out) {
-    while(true) {
-        string product;
-        in >> product;
-        product += "house.\n";
-        out << product;
-    }
+} catch (Channel<string>::ClosedException &e) {
+    out.close();
 }
 
 int main() {
     // make channels and workers
     chan<string> ch1, ch2, ch3;
     chan<string> ch4(10); // some capacity
-    thread(builder,  ch1, ch2).detach();
-    thread(painter,  ch2, ch3).detach();
-    thread(finisher, ch3, ch4).detach();
+    thread t1(worker, ch1, ch2, "two-story ");
+    thread t2(worker, ch2, ch3, "blue ");
+    thread t3(worker, ch3, ch4, "house.\n");
 
     // make orders
     for (size_t i = 0; i != 10; ++i) {
@@ -50,4 +37,10 @@ int main() {
         ch4 >> result;
         cout << result;
     }
+    
+    // clean up workers
+    ch1.close();
+    t1.join();
+    t2.join();
+    t3.join();
 }
